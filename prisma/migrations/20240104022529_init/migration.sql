7c202a643c875ef32f235fa80e742a492b1b4eeb
Dikendev <diken.dev@gmail.com>
2024-01-04 02:30:57 -0300

    add schema migration

---
+++ prisma/migrations/20240104022529_init/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `parentId` on the `Comment` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Comment" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "text" TEXT NOT NULL,
+    "likes" INTEGER NOT NULL DEFAULT 0,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Comment" ("createdAt", "id", "likes", "text", "userId") SELECT "createdAt", "id", "likes", "text", "userId" FROM "Comment";
+DROP TABLE "Comment";
+ALTER TABLE "new_Comment" RENAME TO "Comment";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
