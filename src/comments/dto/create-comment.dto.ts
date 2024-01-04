d4879c8c56dbf2ada8f32ccdd22da375d5a8b31f
Dikendev <diken.dev@gmail.com>
2024-01-04 02:48:32 -0300

    feat add create comment dto

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,1 +1,19 @@
-export class CreateCommentDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateCommentDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  text: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  userId: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  parentId?: string;
+}
