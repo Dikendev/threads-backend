e4b74feabefda3a2027c35582f7b289ce35d1334
Dikendev <diken.dev@gmail.com>
2024-01-05 18:26:52 -0300

    feat add tests create comment

--- src/comments/comments.controller.spec.ts
+++ src/comments/comments.controller.spec.ts
@@ -1,20 +1,58 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
+import { PrismaService } from '../prisma.service';
+import { CreateCommentDto } from './dto/create-comment.dto';
 
 describe('CommentsController', () => {
   let controller: CommentsController;
+  let prisma: PrismaService;
+
+  const text = 'Loren Ipsum Text';
+  const idMock = '111-222-333';
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CommentsController],
-      providers: [CommentsService],
+      providers: [
+        {
+          provide: CommentsService,
+          useValue: {
+            create: jest.fn((data: CreateCommentDto) => ({
+              id: idMock,
+              text: data.text,
+              userId: data.userId,
+              parentId: data.parentId || null,
+              createdAt: new Date(),
+              updatedAt: new Date(),
+              likes: 0,
+            })),
+          },
+        },
+        PrismaService,
+      ],
     }).compile();
 
     controller = module.get<CommentsController>(CommentsController);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('prisma should be defined', () => {
+    expect(prisma).toBeDefined();
+  });
+
+  it('should create a comment with success', async () => {
+    const data: CreateCommentDto = { text, userId: 'mocked-user-id' };
+    const comment = await controller.create(data);
+
+    expect(comment.id).toBeDefined();
+    expect(comment.id).toBe(idMock);
+    expect(comment.text).toBe(data.text);
+    expect(comment.userId).toBe(data.userId);
+    expect(comment.parentId).toBe(null);
   });
 });
