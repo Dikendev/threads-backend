768e3e1576bfb15368c6be39b1216172ec4abc5b
Dikendev <diken.dev@gmail.com>
2024-01-06 23:40:54 -0300

    feat add user service tests

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { PrismaService } from '../prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let userService: UsersService;
+  let prisma: PrismaService;
+  const data: CreateUserDto = { name: 'Diego' };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService, PrismaService],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    userService = module.get<UsersService>(UsersService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
   });
+  it('prisma should be defined', () => {
+    expect(prisma).toBeDefined();
+  });
+
+  it('should create a user with success', () => {
+    const user = prisma.user.create({ data });
+
+    expect(user).resolves.toEqual({
+      id: expect.any(String),
+      name: 'Diego',
+      createdAt: expect.any(Date),
+      updatedAt: expect.any(Date),
+    });
+  });
+
+  it('should get an a user with success', () => {});
 });
