3e07e91a09b584da7727f5b3a2a96ddd83d7ee6e
Dikendev <diken.dev@gmail.com>
2024-01-10 23:34:03 -0300

    feat add get user by id / delete user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,15 +3,18 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from '../prisma.service';
 import { Prisma, User } from '@prisma/client';
+import { ResponseUserDto } from './dto/response-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  async create(data: CreateUserDto): Promise<User> {
-    return this.prisma.user.create({
+  async create(data: CreateUserDto): Promise<ResponseUserDto> {
+    const user = await this.prisma.user.create({
       data,
     });
+
+    return user;
   }
 
   async getUsers(params: {
@@ -31,15 +34,40 @@ export class UsersService {
     });
   }
 
-  async getUser(id: number) {
-    return `This action returns a #${id} user`;
+  async getUser(id: string) {
+    try {
+      const user = await this.prisma.user.findUniqueOrThrow({ where: { id } });
+
+      return user;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new Prisma.PrismaClientKnownRequestError(error?.message, {
+          code: error.code,
+          clientVersion: error?.clientVersion,
+          meta: error?.meta,
+          batchRequestIdx: error?.batchRequestIdx,
+        });
+      } else {
+        // Handle other types of errors
+      }
+    }
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async deleteUser(param: any): Promise<any> {
+    try {
+      const user = await this.prisma.user.delete({
+        where: {
+          email: param,
+          OR: [{ id: param }, { email: param }],
+        },
+      });
+      return user;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }
