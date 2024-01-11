74242d16f0038db6d60397a08eb877d02be226d8
Dikendev <diken.dev@gmail.com>
2024-01-10 23:34:36 -0300

    change moved service tests to controller tests

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { PrismaService } from '../prisma.service';
-import { CreateUserDto } from './dto/create-user.dto';
 
 describe('UsersService', () => {
   let userService: UsersService;
-  let prisma: PrismaService;
-  const data: CreateUserDto = { name: 'Diego' };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,26 +11,9 @@ describe('UsersService', () => {
     }).compile();
 
     userService = module.get<UsersService>(UsersService);
-    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(userService).toBeDefined();
   });
-  it('prisma should be defined', () => {
-    expect(prisma).toBeDefined();
-  });
-
-  it('should create a user with success', () => {
-    const user = prisma.user.create({ data });
-
-    expect(user).resolves.toEqual({
-      id: expect.any(String),
-      name: 'Diego',
-      createdAt: expect.any(Date),
-      updatedAt: expect.any(Date),
-    });
-  });
-
-  it('should get an a user with success', () => {});
 });
