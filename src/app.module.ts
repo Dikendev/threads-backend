761e1e8e6f7cfbb4eb0a344f6d4cb1ff4cc9c65d
Dikendev <diken.dev@gmail.com>
2024-01-10 23:43:45 -0300

    change update prisma exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,15 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CommentsModule } from './comments/comments.module';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
+import { PrismaExceptionFilter } from './prisma-exception/prisma-exception.filter';
 
 @Module({
   imports: [UsersModule, CommentsModule, ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_FILTER, useClass: PrismaExceptionFilter },
+  ],
 })
 export class AppModule {}
