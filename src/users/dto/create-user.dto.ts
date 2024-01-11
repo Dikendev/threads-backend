1f821581a0f174769270fc95a025774cfc6c7995
Dikendev <diken.dev@gmail.com>
2024-01-10 23:31:40 -0300

    change update create user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,11 @@
-import { IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @IsNotEmpty()
   @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
 }
