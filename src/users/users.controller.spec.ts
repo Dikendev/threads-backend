0650704e61eb17faad17ffdacd0a48e7e690c891
Dikendev <diken.dev@gmail.com>
2024-01-13 23:47:17 -0300

    feat add mock user

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -5,7 +5,9 @@ import { PrismaService } from '../prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaExceptionFilter } from '../prisma-exception/prisma-exception.filter';
 import { ResponseUserDto } from './dto/response-user.dto';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { BadRequestException } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -14,7 +16,7 @@ describe('UsersController', () => {
 
   const data: CreateUserDto = {
     name: 'Diego',
-    email: 'diego@gmail.com',
+    email: 'Diego@gmail.com',
   };
 
   const dataEmptyValues = {
@@ -24,19 +26,24 @@ describe('UsersController', () => {
 
   const idMock = '111-222-333';
 
-  const userMock = {
-    create: jest.fn(
-      (data: CreateUserDto): Promise<ResponseUserDto> =>
-        ({
-          id: idMock,
-          name: data.name,
-          email: data.email,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        }) as unknown as Promise<ResponseUserDto>,
-    ),
-    delete: jest.fn(),
+  const result: ResponseUserDto = {
+    id: idMock,
+    name: data.name,
+    email: data.email,
+    createdAt: new Date(),
+    updatedAt: new Date(),
   };
+
+  class MockUsersService {
+    create = jest
+      .fn()
+      .mockResolvedValue(result)
+      .mockRejectedValue(new BadRequestException());
+    delete = jest.fn();
+    getUser = jest.fn();
+  }
+
+  const mockUsersService = new MockUsersService();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -44,7 +51,7 @@ describe('UsersController', () => {
       providers: [
         {
           provide: UsersService,
-          useValue: userMock,
+          useValue: mockUsersService,
         },
         PrismaService,
         PrismaExceptionFilter,
@@ -54,6 +61,10 @@ describe('UsersController', () => {
     controller = module.get<UsersController>(UsersController);
     prisma = module.get<PrismaService>(PrismaService);
     exception = module.get<PrismaExceptionFilter>(PrismaExceptionFilter);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
@@ -68,29 +79,53 @@ describe('UsersController', () => {
     expect(exception).toBeDefined();
   });
 
-  it('should create a user with success', async () => {
-    const user = await controller.create(data);
+  describe('create', () => {
+    it('should create a user with success', async () => {
+      mockUsersService.create.mockResolvedValueOnce(result);
 
-    expect(user.id).toBeDefined();
-    expect(user.id).toBe(idMock);
-    expect(user.createdAt).toStrictEqual(expect.any(Date));
-    expect(user.updatedAt).toStrictEqual(expect.any(Date));
-  });
+      const user = await controller.create(data);
+      expect(mockUsersService.create).toHaveBeenCalledTimes(1);
+      expect(user).toEqual(result);
+    });
 
-  it('should not accept duplicate email', async () => {
-    await expect(prisma.user.create({ data })).rejects.toThrow(
-      PrismaClientKnownRequestError,
-    );
-  });
+    it('should not accept duplicate email', async () => {
+      await expect(controller.create(data)).rejects.toThrow(
+        new BadRequestException(),
+      );
+    });
 
-  it('should not accept empty name || email ', async () => {
-    await expect(await controller.create(dataEmptyValues)).rejects.toThrow(
-      Error,
-    );
+    it('should not accept empty name || email ', async () => {
+      const empty = plainToInstance(CreateUserDto, dataEmptyValues);
+      const errors = await validate(empty);
+
+      console.log(errors, 'errors');
+      expect(errors.length).not.toBe(0);
+    });
   });
 
   it('should delete a user by id', async () => {
     const id = '15accb38-242d-428b-bfc7-d123b7b5a052';
     expect(await controller.deleteUser(id)).toBe('');
+  });
+
+  describe('getUser', () => {
+    it('should get a user by id', async () => {
+      const mockUser: ResponseUserDto = {
+        id: '123',
+        name: 'Diego',
+        email: 'Diego@gmail.teste',
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+      };
+      jest
+        .spyOn(controller, 'getUser')
+        .mockReturnValueOnce(Promise.resolve(mockUser));
+
+      const result = await controller.getUser(mockUser.id);
+
+      expect(result).toBe(mockUser);
+      expect(controller.getUser).toHaveBeenCalledTimes(1);
+      expect(controller.getUser).toHaveBeenCalledWith(mockUser.id);
+    });
   });
 });
