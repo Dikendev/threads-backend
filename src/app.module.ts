83df363427f5e0f2ffb65b261faadd2b46758ba0
Dikendev <diken.dev@gmail.com>
2024-01-16 22:15:31 -0300

    feat add logger to main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -6,13 +6,15 @@ import { CommentsModule } from './comments/comments.module';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
 import { PrismaExceptionFilter } from './prisma-exception/prisma-exception.filter';
+import { LoggerModule } from './utils/logger/logger.module';
 
 @Module({
-  imports: [UsersModule, CommentsModule, ConfigModule.forRoot()],
+  imports: [UsersModule, CommentsModule, LoggerModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [
     AppService,
     { provide: APP_FILTER, useClass: PrismaExceptionFilter },
+    Logger,
   ],
 })
 export class AppModule {}
