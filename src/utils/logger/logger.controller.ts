98abc48600c9c3e40a95431d285a1b18546cc707
Dikendev <diken.dev@gmail.com>
2024-01-16 22:45:59 -0300

    feat add logger controller

--- src/utils/logger/logger.controller.ts
+++ src/utils/logger/logger.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { LoggerService } from './logger.service';
 
 @Controller('logger')
-export class LoggerController {}
+export class LoggerController {
+  CONTROLLER = LoggerController.name;
+  constructor(private readonly logger: LoggerService) {}
+
+  @Get('info')
+  getInfoLog(): string {
+    this.logger.log('Log info sample', this.CONTROLLER);
+    return 'Logged an INFO message.';
+  }
+
+  @Get('error')
+  getErrorLog(): string {
+    this.logger.error('Log error sample', null, this.CONTROLLER);
+    return 'Logged an ERROR message.';
+  }
+}
