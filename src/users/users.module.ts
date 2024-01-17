2bc872c5de802a96732b6f64f24f2b14a9deb574
Dikendev <diken.dev@gmail.com>
2024-01-16 22:15:35 -0300

    feat add logger to main

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService],
+  providers: [UsersService, PrismaService, Logger],
 })
 export class UsersModule {}
