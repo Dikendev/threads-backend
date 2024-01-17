9195153f1bae8824504013865cdc8f57c2bf3f45
Dikendev <diken.dev@gmail.com>
2024-01-16 22:45:46 -0300

    feat add logger service

--- src/utils/logger/logger.service.ts
+++ src/utils/logger/logger.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { instance as logger } from './winston.logger';
 
 @Injectable()
-export class LoggerService {}
+export class LoggerService {
+  log(message: string, context?: string) {
+    logger.info(message, { context });
+  }
+
+  error(message: string, trace: string, context?: string) {
+    logger.error(message, { context, trace });
+  }
+
+  warn(message: string, context?: string) {
+    logger.warn(message, { context });
+  }
+
+  debug(message: string, context?: string) {
+    logger.debug(message, { context });
+  }
+}
